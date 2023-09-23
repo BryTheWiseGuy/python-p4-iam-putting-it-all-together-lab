87d26db60611b110a7befb79782b6390c58382db
Brian Ashman <brian.ashman@comcast.net>
2023-09-22 21:08:11 -0700

    Update Lab Progress

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import request, session
+from flask import request, session, jsonify, make_response
 from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
 
@@ -8,10 +8,32 @@ from config import app, db, api
 from models import User, Recipe
 
 class Signup(Resource):
-    pass
+    def post(self):
+        json_data = request.get_json()
+        
+        new_user = User(
+            username = json_data.get('username'),
+            bio = json_data.get('bio'),
+            image_url = json_data.get('image_url'),
+            _password_hash = json_data.get('password')
+        )
+        
+        db.session.add(new_user)
+        db.session.commit()
+        
+            
+        session['user_id'] = new_user.id
+        new_user_dict = new_user.to_dict()
+
+        return make_response(jsonify(new_user_dict), 201)
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+        if user:
+            return make_response(user.to_dict(), 200)
+        else:
+            return {"message": "Unauthorized"}, 401
 
 class Login(Resource):
     pass
